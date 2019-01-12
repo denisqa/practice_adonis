f45542a25f3b8641979ec88bd479713503bef36f
Denys Kholiavka <d.kholiavka@gmail.com>
2019-01-13 00:21:04 +0200

    Fixed seeders

--- database/seeds/03_AttributeSeeder.js
+++ database/seeds/03_AttributeSeeder.js
@@ -6,17 +6,12 @@ class AttributeSeeder {
   async run() {
     await Attribute.query().delete();
 
-    const carType = await Type.findBy('name', 'car');
-    const carAttributes = await Factory.model('App/Models/Attribute').createMany(3, carType.id);
-    // await carType.attribute().createMany(carAttributes);
+    const types = await Type.all();
+    const typesJSON = types.toJSON();
 
-    const laptopType = await Type.findBy('name', 'laptop');
-    const laptopAttributes = await Factory.model('App/Models/Attribute').createMany(3, laptopType.id);
-    // await laptopType.attribute().createMany(laptopAttributes);
-
-    const phoneType = await Type.findBy('name', 'phone');
-    const phoneAttributes = await Factory.model('App/Models/Attribute').createMany(1, phoneType.id);
-    // await phoneType.attribute().createMany(phoneAttributes);
+    for (const type of typesJSON) {
+      Factory.model('App/Models/Attribute').createMany(3, type.id);
+    }
   }
 }
 
