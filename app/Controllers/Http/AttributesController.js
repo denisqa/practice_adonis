db735f33e7016f0c6426623201f087d9802d5917
Denys Kholiavka <d.kholiavka@gmail.com>
2019-01-12 13:19:00 +0200

    Fixed attribute controller

--- app/Controllers/Http/AttributesController.js
+++ app/Controllers/Http/AttributesController.js
@@ -1,32 +1,33 @@
 const Attribute = use('App/Models/Attribute');
 class AttributesController{
-	async loadAllAttributes(){
-        return Attribute.all();
-    }
-    
-    async loadAttributes({ request, params }){
-        const { id } = params;
-        return Attribute.findOrFail(id);
-    }
+	async loadAllAttributes({ params }){
+		const { type_id: typeId } = params;
+		return Attribute.findAttributes(typeId);
+	}
+	
+	async loadAttribute({ request, params }){
+		const { type_id: typeId, id } = params;
+		return Attribute.findAttribute(typeId, id);
+	}
 
-    async addAttribute({ request, response }){
-		const data = request.all();
-        await Attribute.add(data);
-        return response.json();
-    }
+	async addAttribute({ request, response, params }){
+		const { type_id: typeId } = params;
+		const { name } = request.all();
+		response.status(201);
+		return await Attribute.add(typeId, name);;
+	}
 
-    async updateAttribute({ request, response, params }){
-		const { id } = params;
-		const data = request.all();
-        await Attribute.update(id, data);
-        return response.json({result: `Attribute with id=${id} updated`});
-    }
+	async updateAttribute({ request, params }){
+		const { type_id: typeId, id } = params;
+		const { name } = request.all();
+		return await Attribute.update(typeId, id, name);
+	}
 
-    async deleteAttribute({ params, response }){
-		const { id } = params;
-        await Attribute.delete(id);
-        return response.json({result: `Attribute with id=${id} deleted`});
-    }
+	async deleteAttribute({ params, response }){
+		const { type_id: typeId, id } = params;
+		await Attribute.delete(typeId, id);
+		return response.json({message: `Attribute with id=${id} deleted`});
+	}
 }
 
 module.exports = AttributesController;
