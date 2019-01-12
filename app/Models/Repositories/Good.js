8d3c5e949a0f0c2006381545b47c79a65233ec6d
Denys Kholiavka <d.kholiavka@gmail.com>
2019-01-12 17:40:30 +0200

    Fixed repositories

--- app/Models/Repositories/Good.js
+++ app/Models/Repositories/Good.js
@@ -2,18 +2,19 @@ const Model = use('App/Models/Good');
 const Attribute = use('App/Models/Attribute');
 
 class Good {
-    static async addAttribute(good, attributes){
-        for(const attr of attributes){
-            for(let key in attr){
-                good.attribute().attach(key, (row) => {
-                    row.value = attr[key];
-                });
-            }
-        }
+  static async addAttribute(good, attributes) {
+    for (const attr of attributes) {
+      for (const key in attr) {
+        good.attribute().attach(key, row => {
+          row.value = attr[key];
+        });
+      }
     }
+  }
+
   static async add(data) {
     const { name, price, user_id: userId, type_id: typeId, attributes } = data;
-    const good = await this.create({ name: name, price: price, user_id: userId, type_id: typeId });
+    const good = await this.create({ name, price, user_id: userId, type_id: typeId });
     good.attributes = await Good.addAttribute(good, attributes);
     return good;
   }
