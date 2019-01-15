10c3b459bcd0297ce5e94664d7ea9096f3b1ed97
Denys Kholiavka <d.kholiavka@gmail.com>
2019-01-16 00:19:47 +0200

    Added validators

--- start/routes.js
+++ start/routes.js
@@ -6,23 +6,23 @@ Route.get('login/', 'AuthController.login').middleware('guest');
 
 /* only admin */
 Route.group(() => {
-    Route.get('/types/:type_id/attributes', 'AttributesController.loadAllAttributes');
-    Route.get('/types/:type_id/attributes/:id', 'AttributesController.loadAttributes');
-    Route.post('/types/:type_id/attributes', 'AttributesController.addAttribute');
-    Route.put('/types/:type_id/attributes/:id', 'AttributesController.updateAttribute');  
-    Route.delete('/types/:type_id/attributes/:id', 'AttributesController.deleteAttribute');
-    
-    Route.get('/types', 'TypesController.loadAllTypes');
-    Route.get('/types/:id', 'TypesController.loadTypes');
-    Route.post('/types', 'TypesController.addType');
-    Route.put('/types/:id', 'TypesController.updateType');  
-    Route.delete('/types/:id', 'TypesController.deleteType');
+  Route.get('/types/:type_id/attributes', 'AttributesController.loadAllAttributes');
+  Route.get('/types/:type_id/attributes/:id', 'AttributesController.loadAttributes');
+  Route.post('/types/:type_id/attributes', 'AttributesController.addAttribute').validator('AddUpdateAttribute');
+  Route.put('/types/:type_id/attributes/:id', 'AttributesController.updateAttribute').validator('AddUpdateAttribute');
+  Route.delete('/types/:type_id/attributes/:id', 'AttributesController.deleteAttribute');
+
+  Route.get('/types', 'TypesController.loadAllTypes');
+  Route.get('/types/:id', 'TypesController.loadTypes');
+  Route.post('/types', 'TypesController.addType').validator('AddUpdateType');
+  Route.put('/types/:id', 'TypesController.updateType').validator('AddUpdateType');
+  Route.delete('/types/:id', 'TypesController.deleteType');
 });
 
 Route.get('/goods', 'GoodsController.loadAllGoods');
 Route.get('/goods/:id', 'GoodsController.loadGoods');
 
 /* only auth and admin */
-Route.post('/goods', 'GoodsController.addGoods');
-Route.put('/goods/:id', 'GoodsController.updateGoods'); 
-Route.delete('/goods/:id', 'GoodsController.deleteGood'); 
+Route.post('/goods', 'GoodsController.addGoods').validator('AddGood');
+Route.put('/goods/:id', 'GoodsController.updateGoods').validator('UpdateGood');
+Route.delete('/goods/:id', 'GoodsController.deleteGood');
